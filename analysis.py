150a543cc2fb57ac41700ddaac6cdf98a1002928
Robert Balaban <rob.balaban@gmail.com>
2017-05-18 01:39:25 +0300

    Added all fields to the table

--- analysis.py
+++ analysis.py
@@ -49,13 +49,70 @@ def rel_realgdp():
 
     return intermList
 
+# GDP Deflator related to previous year
 
+def gdp_deflatorprev():
+    intermList = []
+
+    intermList.append((100))
+    for x in xrange(1, len(data)):
+        intermList.append((data.RELATIONGDPI[x] / data.RELATIONREALGDP[x]))
+
+    return intermList
+
+# Inflation Rate related to specific year
+
+def inflation_ratabase():
+    intermList = []
+
+    intermList.append((100))
+    for x in xrange(1, len(data)):
+        intermList.append((data.GDPDEFLATOR[x] - 1))
+
+    return intermList
+
+
+# Inflation rate related to previous year
+
+def inflation_rataprev():
+    intermList = []
+
+    intermList.append((100))
+    for x in xrange(1, len(data)):
+        intermList.append((data.GDPDEFLATORPREV[x] - 1))
+
+    return intermList
+
+# Real gdp related to base year
+
+def realgdp_base():
+    intermList = []
+
+    intermList.append((100))
+    for x in xrange(1, len(data)):
+        intermList.append((data.GDPI[x] / data.GDPDEFLATOR[x]))
+
+    return intermList
+
+# Real gdp relate to previous year
+
+def realgdp_prev():
+    intermList = []
+
+    intermList.append((100))
+    for x in xrange(1, len(data)):
+        intermList.append((data.GDPI[x] / data.GDPDEFLATORPREV[x]))
+
+    return intermList
 
 data['NOMINALGDP'] = nominal_gdp()
 data['GDPDEFLATOR'] = gdp_deflator()
 data['RELATIONGDPI'] = rel_gdpi()
 data['RELATIONREALGDP'] = rel_realgdp()
-
-
+data['GDPDEFLATORPREV'] = gdp_deflatorprev()
+data['INFLATIONRATEBASE'] = inflation_ratabase()
+data['INFLATIONRATEPREV'] = inflation_rataprev()
+data['REALGDPBASE'] = realgdp_base()
+data['REALGDPPREV'] = realgdp_prev()
 
 print(data)
